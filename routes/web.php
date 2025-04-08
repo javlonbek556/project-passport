8f2fda881b2c9d958c5b27870f32ab65f4c53f74
javlonbek556 <javlonzdess@gmail.com>
2025-04-08 10:23:51 +0500

    router codes in here

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,34 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PassportController;     
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\PassportController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.register');
 });
 
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 
-// Authenticated user routes
-Route::middleware(['auth'])->group(function () {
-    Route::resource('/passport', PassportController::class)->names('passport');
-});
 
-// Guest routes (login/register)
 Route::middleware(['guest'])->group(function () {
-    Route::get('/login', [UserController::class, 'index'])->name('login'); // login form
-    Route::post('/login', [UserController::class, 'login'])->name('login.perform'); // login submit
-
-    Route::get('/home', function () {
-        return view('welcome'); // yoki dashboard sahifa
-    })->name('home'); // <<== home routeni to‘g‘ri yopish
-    
+    Route::get('/login', [UserController::class, 'index'])->name('users.index');
+    Route::post('/login', [UserController::class, 'login'])->name('login');
     Route::resource('/users', UserController::class)->names('users');
 });
 
-// Passport-specific middleware
+
 Route::middleware(['auth', 'check.passport'])->group(function () {
     Route::get('/passport/create', [PassportController::class, 'create'])->name('passport.create');
     Route::post('/passport', [PassportController::class, 'store'])->name('passport.store');
+});
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/passport', [PassportController::class, 'index'])->name('passport.index');
+    Route::get('/passport/{id}', [PassportController::class, 'show'])->name('passport.show');
+    Route::get('/passport/{id}/edit', [PassportController::class, 'edit'])->name('passport.edit');
+    Route::put('/passport/{id}', [PassportController::class, 'update'])->name('passport.update');
+    Route::delete('/passport/{id}', [PassportController::class, 'destroy'])->name('passport.destroy');
 });
